e4580f9612e8e15f0220819d6a0e8ff4778c7cef
Nabil Hadi <napilfadl@gmail.com>
2022-04-17 20:13:14 +0300

    add About component

---
+++ src/components/About.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/About.css";
+
+export default class About extends React.Component {
+  render() {
+    return <div className="About">About</div>;
+  }
+}
--- src/components/CV.js
+++ src/components/CV.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "../styles/CV.css";
+import About from "./About";
 
 export default class CV extends React.Component {
   render() {
-    return <div className="CV">CV</div>;
+    return (
+      <div className="CV">
+        <About />
+      </div>
+    );
   }
 }
