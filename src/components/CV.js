4e972cf642395ee1675ba273a193ef2a049e2cbc
Nabil Hadi <napilfadl@gmail.com>
2022-04-17 20:16:32 +0300

    add Contact component

--- src/components/CV.js
+++ src/components/CV.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "../styles/CV.css";
 import About from "./About";
+import Contact from "./Contact";
 
 export default class CV extends React.Component {
   render() {
     return (
       <div className="CV">
         <About />
+        <Contact />
       </div>
     );
   }
---
+++ src/components/Contact.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/Contact.css";
+
+export default class Contact extends React.Component {
+  render() {
+    return <div className="Contact">Contact</div>;
+  }
+}
