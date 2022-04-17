322231d977c3e09eaebb318262f5518b96b7ade6
Nabil Hadi <napilfadl@gmail.com>
2022-04-17 04:06:38 +0300

    change app component to class

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-function App() {
-  return <div className="App"></div>;
-}
+import React from "react";
 
-export default App;
+export default class App extends React.Component {
+  render() {
+    return <div className="App"></div>;
+  }
+}
