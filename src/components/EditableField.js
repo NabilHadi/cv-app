33a92c6767e9265af04a5cfa2736c24dc8b5847f
Nabil Hadi <napilfadl@gmail.com>
2022-04-20 03:55:45 +0300

    convert EditableField to functional

--- src/components/EditableField.js
+++ src/components/EditableField.js
@@ -1,24 +1,24 @@
 import React from "react";
 
-export default class EditableField extends React.Component {
-  render() {
-    return (
-      <div className={this.props.className}>
-        {this.props.icon}
-        <input
-          className={this.props.className}
-          autoFocus
-          placeholder={this.props.placeholder}
-          value={this.props.value}
-          onChange={this.props.onFieldChange}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              this.props.onFieldSubmit(e);
-            }
-          }}
-          onBlur={this.props.onFieldSubmit}
-        />
-      </div>
-    );
-  }
-}
+const EditableField = (props) => {
+  return (
+    <div className={props.className}>
+      {props.icon}
+      <input
+        className={props.className}
+        autoFocus
+        placeholder={props.placeholder}
+        value={props.value}
+        onChange={props.onFieldChange}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            props.onFieldSubmit(e);
+          }
+        }}
+        onBlur={props.onFieldSubmit}
+      />
+    </div>
+  );
+};
+
+export default EditableField;
